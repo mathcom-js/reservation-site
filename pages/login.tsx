8845e2ba8897b9190775b5e982ef8328b55e642d
Junhyuk Lee <ljhjoon00@gmail.com>
2022-03-30 15:13:15 +0900

    modify: Change style of login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,58 +1,80 @@
-import { useState } from "react";
-
-function cls(...classnames: string[]) {
-  return classnames.join(" ");
-}
-
 export default function Login() {
-  const [method, setMethod] = useState("admin");
-  const onAdminClick = () => {
-    setMethod("admin");
-  };
-  const onGuestClick = () => {
-    setMethod("guest");
-  };
   return (
-    <div
-      className="max-w-md w-full flex flex-col mx-auto
-     mt-32 space-y-4 bg-slate-50 shadow-lg rounded-2xl pb-4"
-    >
-      <div className="flex justify-center">
+    <div className="max-w-xl w-full flex flex-col mx-auto mt-20 space-y-4">
+      <h1 className="text-center text-lg">Make Your Reservation!</h1>
+      <div className="grid grid-cols-2">
+        <span className="text-center text-sm pt-4 pb-2 text-gray-600 border-b border-gray-200">
+          Admin
+        </span>
+        <span className="text-center text-sm pt-4 pb-2 text-gray-600 border-b border-gray-200">
+          Guest
+        </span>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <span className="text-xs">ID</span>
+        <input
+          className="focus:outline-none focus:border-orange-400 border-2 border-gray-200 rounded-md pl-1.5"
+          type="text"
+        />
+      </div>
+      <div className="flex flex-col space-y-2">
+        <span className="text-xs">Password</span>
+        <input
+          className="focus:outline-none focus:border-orange-400 border-2 border-gray-200 rounded-md pl-1.5"
+          type="password"
+        />
+      </div>
+      <button
+        className="text-xs bg-orange-500 rounded-md py-2 text-white
+      hover:bg-orange-600 transition-colors"
+      >
+        Log in
+      </button>
+      <div>
+        <div className="border-t border-gray-200 w-full relative top-3.5 -z-10" />
+        <div className="text-center">
+          <span className="text-xs text-gray-500 bg-white px-3">
+            No account?
+          </span>
+        </div>
+      </div>
+      <button
+        className="text-xs bg-orange-500 rounded-md py-2 text-white
+       hover:bg-orange-600 transition-colors"
+      >
+        Sign in
+      </button>
+      <div>
+        <div className="border-t border-gray-200 w-full relative top-3.5 -z-10" />
+        <div className="text-center">
+          <span className="text-xs text-gray-500 bg-white px-3">Or use</span>
+        </div>
+      </div>
+      <div className="grid grid-cols-2">
         <button
-          onClick={onAdminClick}
-          className={cls(
-            "w-full py-1.5",
-            method === "admin" ? "" : "bg-slate-700 rounded-tl-2xl"
-          )}
+          className="flex items-center justify-center border border-gray-200 py-2 text-xs text-gray-400
+           hover:bg-slate-50 transition-colors rounded-md"
         >
-          Login as admin
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+              clipRule="evenodd"
+            />
+          </svg>
         </button>
         <button
-          onClick={onGuestClick}
-          className={cls(
-            "w-full py-1.5",
-            method === "guest" ? "" : "bg-slate-700 rounded-tr-2xl"
-          )}
+          className="flex items-center justify-center border border-gray-200 py-2 text-xs text-gray-400
+           hover:bg-slate-50 transition-colors rounded-md"
         >
-          Login as guest
+          KaKao
         </button>
       </div>
-      <form className="flex flex-col items-center space-y-4">
-        <div className="flex flex-col text-sm w-1/2">
-          <span>Username</span>
-          <input type="text" className="border-2 border-gray-300" />
-        </div>
-        <div className="flex flex-col text-sm w-1/2">
-          <span>Password</span>
-          <input type="password" className="border-2 border-gray-300" />
-        </div>
-        <button className="w-1/2 bg-green-600 shadow-md rounded-md py-1 text-white">
-          Log in
-        </button>
-      </form>
-      <button className="w-1/2 bg-blue-600 shadow-md rounded-md py-1 text-white mx-auto">
-        Sign up
-      </button>
     </div>
   );
 }

dfeb98d64a6bddb4b030c23afaa2ca046c9e60e2
Junhyuk Lee <ljhjoon00@gmail.com>
2022-03-31 01:45:11 +0900

    add: Add Footer and apply to every page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+const Footer = () => {
+  return (
+    <div
+      className="fixed bottom-0 bg-slate-700 text-white w-full
+                 flex items-center justify-center py-2"
+    >
+      &copy; 2022. JGJ all rights reserved.
+    </div>
+  );
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;

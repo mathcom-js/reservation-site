7796f89e05a263741347e03e1af99d6eab8f0688
Junhyuk Lee <ljhjoon00@gmail.com>
2022-05-08 19:14:12 +0900

    chore: Typo

--- components/ShopThumbnail.tsx
+++ components/ShopThumbnail.tsx
@@ -81,9 +81,9 @@ const ShopThumbnail = ({
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               <span>{heartCount}</span>

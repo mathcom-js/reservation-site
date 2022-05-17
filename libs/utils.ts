c532b8fe8fca950cfcd0009249cbb528e8b64f11
Junhyuk Lee <ljhjoon00@gmail.com>
2022-05-17 20:37:51 +0900

    add: Make image utility function

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,3 +1,9 @@
 export function cls(...classnames: string[]) {
   return classnames.join(" ");
 }
+
+export function createImageUrl(id: string, variants: string | undefined) {
+  return variants
+    ? `https://imagedelivery.net/BDH_sV5MMFDjmj9Ky8ZKTQ/${id}/${variants}`
+    : `https://imagedelivery.net/BDH_sV5MMFDjmj9Ky8ZKTQ/${id}/public`;
+}

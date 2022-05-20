47f1ee3794b8f6b97f7502ef0648f5c821bedd58
Leegon, Kim <juliette285@gmail.com>
2022-05-21 02:32:27 +0900

    style: Change the code name

--- pages/api/codes.ts
+++ pages/api/codes.ts
@@ -1,19 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const Redirected_Uri = "http://localhost:3000";
+// REST_API_KEY → Client ID
+const Redirected_Uri = "http://localhost:3000/RedirectHandler";
 const REST_API_KEY = process.env.KAKAO_REST_API_KEY;
-const CLIENT_SECRET = process.env.KAKAO_CLIENT_SECRET;
 const login_url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${Redirected_Uri}`;
 
-export default async function KakaoCodes(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function Codes(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    res.redirect(login_url);
+    await res.redirect(login_url);
   } else {
     res.status(405).end();
   }

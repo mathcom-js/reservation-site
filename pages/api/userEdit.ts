ad72ef7f26d7edb75e8df86b644f7f89ee82e5f2
Chosamee <48636376+Chosamee@users.noreply.github.com>
2022-05-23 14:54:21 +0900

    add: Create user edit function

---
+++ pages/api/userEdit.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    const {
+      body: { username, id },
+    } = req;
+    try {
+      console.log(id);
+      const block =
+        id.length > 0
+          ? {
+              username,
+              avatar: id,
+            }
+          : { username };
+
+      const updatedUser = await client.user.update({
+        where: {
+          id: 5,
+        },
+        data: block,
+      });
+
+      res.json({ ok: true, updatedUser });
+    } catch (error) {
+      res.json({ ok: false, error });
+    }
+  }
+}
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,18 +1,81 @@
 import Header from "../../components/Header";
+import { User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import { Input } from "../../components/Input";
 
-const FAKE_DATA = {
-  id: 1,
-  username: "Sinclairr",
-  shops: [{ id: 1, name: "Test1" }],
-  reviews: [{ id: 1, review: "This Was good" }],
-};
+interface EditForm {
+  username: string;
+  avatar?: FileList;
+}
+
+interface EditReturn {
+  data: {
+    ok: boolean;
+    updatedUser?: User;
+    error?: any;
+  };
+}
 
 export default function EditProfile() {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<EditForm>();
+  const [id, setId] = useState("");
+
+  const onValid = async ({ username, avatar }: EditForm) => {
+    let block;
+    if (avatar && avatar.length > 0) {
+      const {
+        data: { uploadURL },
+      } = await axios.get("/api/image");
+
+      const formData = new FormData();
+      formData.append("file", avatar[0], "test");
+      const { data } = await axios.post(uploadURL, formData);
+      block = { username, id: data.result.id };
+    } else {
+      block = { username };
+    }
+    const { data }: EditReturn = await axios.put("/api/userEdit", block);
+
+    if (!data.updatedUser) {
+      console.log(JSON.stringify(data.error));
+    } else {
+      router.push("/profile");
+    }
+  };
+
   return (
     <>
       <Header />
-      <div className="mt-20">{FAKE_DATA.username}</div>
 
+      <div className="max-w-xl w-full flex flex-col mx-auto mt-20 space-y-8">
+        <h1 className="text-center text-lg text-violet-400">
+          Edit Your Profile
+        </h1>
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex flex-col space-y-8"
+        >
+          <Input
+            register={register("username", { required: true })}
+            name="Username"
+          />
+          <input {...register("avatar")} type="file" accept="image/*" />
+          {id ? (
+            <img
+              src={`https://imagedelivery.net/BDH_sV5MMFDjmj9Ky8ZKTQ/${id}/avatar`}
+            />
+          ) : (
+            <div></div>
+          )}
+
+          <Button text="Submit" />
+        </form>
+      </div>
       <div></div>
     </>
   );

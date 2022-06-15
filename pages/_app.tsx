40f2680043d1c0edd3954aff9e81dde358865691
Junhyuk Lee <38907328+Sinclairr08@users.noreply.github.com>
2022-06-16 00:11:14 +0900

    fix: Solve a script component inside _document.js error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         fetcher: (url: string) => axios.get(url).then((res) => res.data),
       }}
     >
+      <script defer src="https://developers.kakao.com/sdk/js/kakao.js" />
+      <script
+        defer
+        src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"
+      />
       <title>Make Your Reservation!</title>
       <div className="my-20">
         <Component {...pageProps} />
